01ba284ba29aca20b196ee9de3113675f9f89375
Jon Daniel <jon.daniel@salesforce.com>
2025-08-02 23:49:57 -0400

    Moving code to evaluation

--- lib/tobias/evaluation.rb
+++ lib/tobias/evaluation.rb
@@ -5,5 +5,44 @@ module Tobias
     def initialize(database)
       @database = database
     end
+
+    def run(options, &block)
+      WorkMem.all.each do |value|
+        database.transaction do
+          database.run("SET LOCAL work_mem = '#{value.to_sql}'")
+
+          instance_eval(&block)
+
+          @queries.each do |name, block|
+            database.select(Sequel.function(:pg_stat_reset)).first
+
+            query = database.instance_eval(&block)
+            times = []
+
+            options[:iterations].to_i.times do
+              time = Benchmark.realtime do
+                database.run(query.sql)
+              end
+              times << time
+            end
+
+            stats = database[:pg_stat_database].where(datname: Sequel.function(:current_database)).first
+
+            if stats[:temp_files] == 0 && stats[:temp_bytes] == 0
+              return value
+            end
+          end
+        end
+      end
+    end
+
+    private
+
+    attr_reader :database
+
+    def query(name, &block)
+      @queries ||= {}
+      @queries[name] = block
+    end
   end
 end
--- lib/tobias/version.rb
+++ lib/tobias/version.rb
@@ -2,4 +2,7 @@
 
 module Tobias
   VERSION = "0.1.0"
+
+  SUMMARY = "Tobias is a tool to help you find the optimal work_mem for your queries."
+  DESCRIPTION = "Tobias is a tool to help you find the optimal work_mem for your queries."
 end
