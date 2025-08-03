1d5d1ebab425136affce7d35f7fa5a92008ab335
Jon Daniel <jon.daniel@salesforce.com>
2025-08-03 09:59:38 -0400

    Message cleanup

--- lib/tobias/cli.rb
+++ lib/tobias/cli.rb
@@ -27,6 +27,11 @@ module Tobias
       work_mems = WorkMem.valid_for(database)
       results = {}
 
+      parsed = TTY::Markdown.parse(<<~MARKDOWN)
+        # @tobias is thinking...
+      MARKDOWN
+      puts parsed
+
       thinking_time = Benchmark.realtime do
         container.queries.each do |name, block|
           work_mem = Evaluation.new(database, work_mems).run(options, &block)
@@ -36,13 +41,15 @@ module Tobias
       end
 
       parsed = TTY::Markdown.parse(<<~MARKDOWN)
-        # Tobias has sent you a new message
+        # @tobias has sent you a new message
 
         I thought about your queries for #{thinking_time.round(2)} seconds and here is what I recommend:
 
         | Query | Required work_mem |
         |-------|-------------------|
         #{results.map { |name, work_mem| "| #{name} | #{work_mem.to_sql} |" }.join("\n")}
+
+        Your application will need to run with at least #{results.values.max.to_sql} of work_mem.
 
         To apply my recommendations, run the following SQL:
 
