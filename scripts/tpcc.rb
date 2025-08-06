87fbccd145f21f790a93f5095de9eaacf8b756d1
Jon Daniel <jon.daniel@salesforce.com>
2025-08-05 22:31:56 -0400

    Adding more variability

--- scripts/tpcc.rb
+++ scripts/tpcc.rb
@@ -6,13 +6,16 @@ query(:stock_by_warehouse_and_district) do
     limit(1).
     first[:w_id]
 
-  district_id = from(:district).where(d_w_id: warehouse_id).first[:d_id]
-  threshold = 500
+  district_id = from(:district).
+    where(d_w_id: warehouse_id).
+    order(Sequel.lit("RANDOM()")).
+    limit(1).
+    first[:d_id]
 
   from(:stock).
     join(:order_line, ol_w_id: :s_w_id, ol_i_id: :s_i_id).
     where(ol_w_id: warehouse_id, ol_d_id: district_id).
-    where(Sequel.lit("s_quantity < ?", threshold)).
+    where(Sequel.lit("s_quantity < ?", rand(100..500))).
     order(:s_quantity).
     group(:s_i_id, :s_quantity).
     limit(2_000).
