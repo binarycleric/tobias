9d707bb6a06ff33d284d9123cc95f4caa169b0f0
Jon Daniel <binarycleric@users.noreply.github.com>
2025-08-06 22:56:52 -0400

    Adding a parameter that recommends work_mem given a database configuration

--- lib/tobias/cli.rb
+++ lib/tobias/cli.rb
@@ -8,6 +8,37 @@ module Tobias
       true
     end
 
+    desc "recommend", "recommend a work_mem setting for a database"
+    option :database_url, type: :string, required: true
+    option :debug, type: :boolean, default: false
+    def recommend
+      database = Sequel.connect(options[:database_url])
+      database.loggers << Logger.new(STDERR) if options[:debug]
+
+      parsed = TTY::Markdown.parse(<<~MARKDOWN)
+        # @tobias is thinking...
+      MARKDOWN
+      puts parsed
+
+      work_mem = Tobias::WorkMem.valid_for(database).sort_by(&:amount).reverse.first
+
+      parsed = TTY::Markdown.parse(<<~MARKDOWN)
+        # @tobias has sent you a new message
+
+        I've reviewed your database by analyzing your shared buffers and connection limits and
+        I recommend setting `work_mem` to `#{work_mem.to_sql}`. To apply my recommendations, run the following SQL:
+
+        ```sql
+        ALTER SYSTEM SET work_mem = '#{work_mem.to_sql}';
+        SELECT pg_reload_conf();
+        ```
+
+        Regards,
+        ~ Tobias
+      MARKDOWN
+      puts parsed
+    end
+
     desc "profile SCRIPT", "profile"
     option :database_url, type: :string, required: true
     option :debug, type: :boolean, default: false
