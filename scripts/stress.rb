8f0dddec6365f904dd5e3de0f6e0e92b85f97a84
Jon Daniel <binarycleric@users.noreply.github.com>
2025-08-16 23:25:06 -0400

    Fixing workmem_stress

--- scripts/stress.rb
+++ scripts/stress.rb
@@ -1,25 +1,22 @@
 # frozen_string_literal: true
 
-option(:total_rows, 10_000_000)
+option(:total_rows, 1_000_000)
 
 setup do
-  create_table? :workmem_stress do
+  db.create_table? :workmem_stress do
     primary_key :id
     column :name, String
     column :value, Integer
     column :payload, String
+    column :created_at, :timestamp, default: Sequel::CURRENT_TIMESTAMP
   end
-end
 
-load_data do
-  loop do
-    break if from(:workmem_stress).count >= options.total_rows
-
-    100.times do
-      from(:workmem_stress).multi_insert(1000.times.map do
+  run_parallel do
+    (options.total_rows / Etc.nprocessors / 1000).times do
+      db.from(:workmem_stress).multi_insert(1000.times.map do
         {
-          name: "name_#{Random.rand(1..1000)}",
-          value: Random.rand(1..10_000),
+          name: "name_#{Random.rand(1..1_000_000)}",
+          value: Random.rand(1..1_000_000),
           payload: SecureRandom.hex(128)
         }
       end)
@@ -28,26 +25,37 @@ load_data do
 end
 
 teardown do
-  drop_table(:workmem_stress)
+  db.drop_table(:workmem_stress)
 end
 
 query(:large_sort) do
-  from(:workmem_stress)
-    .select(:id, :name, :value, :payload)
-    .order(Sequel.desc(:payload))
-    .limit(10_000)
+  db.
+    from(:workmem_stress).
+    select(:id, :name, :value, :payload).
+    order(Sequel.desc(:payload)).
+    limit(10_000)
+end
+
+query(:large_sort_created_at) do
+  db.
+    from(:workmem_stress).
+    select(:id, :name, :value, :payload).
+    order(Sequel.desc(:created_at)).
+    limit(10_000)
 end
 
 query(:hash_aggregation) do
-  from(:workmem_stress)
-    .select(:name, Sequel.function(:avg, :value))
-    .select { count("*") }
-    .group(:name)
-    .limit(10_000)
+  db.
+    from(:workmem_stress).
+    select(:name, Sequel.function(:avg, :value)).
+    select { count("*") }.
+    group(:name).
+    limit(10_000)
 end
 
 query(:self_join) do
-  from(Sequel.as(:workmem_stress, :a)).
+  db.
+    from(Sequel.as(:workmem_stress, :a)).
     join(Sequel.as(:workmem_stress, :b), id: :id).
     where { Sequel[:a][:id] < 1000 }.
     where { Sequel[:b][:id] < 1000 }.
