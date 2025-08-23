dd3da52998528ec42f719c94435245cc95e3e5ee
Jon Daniel <binarycleric@users.noreply.github.com>
2025-08-23 09:49:40 -0400

    Bumping to version 0.4

--- lib/tobias/version.rb
+++ lib/tobias/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Tobias
-  VERSION = "0.3.0"
+  VERSION = "0.4.0"
   SUMMARY = "Tobias is a tool to help you find the optimal work_mem for your queries."
   DESCRIPTION = "Tobias is a tool to help you find the optimal work_mem for your queries."
 end
